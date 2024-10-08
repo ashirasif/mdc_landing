4f30190ea433fe90061f7011a5d9766ba9c03443
Faisal Hanif <faisal.haniffizz@gmail.com>
2024-10-08 21:03:26 +0500

    border glow animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,19 @@ export default {
       },
       boxShadow: {
         heroBtn: '0px 0px 20px 5px rgba(366, 366, 366, 0.3)',
-      }
+      },
+      animation: {
+        circulate: 'circulate 4s linear infinite',  // New animation for circulating glow
+      },
+      keyframes: {
+        circulate: {
+          '0%': { 'box-shadow': '0 0 2px white, inset 0 0 2px white' },
+          '25%': { 'box-shadow': '2px 0 5px white, inset 2px 0 5px white' },
+          '50%': { 'box-shadow': '0 2px 5px white, inset 0 2px 5px white' },
+          '75%': { 'box-shadow': '-2px 0 5px white, inset -2px 0 5px white' },
+          '100%': { 'box-shadow': '0 -2px 2px white, inset 0 -2px 2px white' },
+        },
+      },
     },
   },
   plugins: [],
